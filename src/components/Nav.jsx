f599f44a2628f3b5af295fc076fb2f2af050f5e4
ZolayvarE <ZolayvarECU@gmail.com>
2016-08-12 14:53:15 -0700

    Renders the page to the DOM

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
-var Nav = () => (
+class Nav extends React.Component {
+
+
+render (
   <nav className="navbar">
     <div className="col-md-6 col-md-offset-3">
       <Search />
     </div>
   </nav>
 );
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,22 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+class VideoListEntry extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render (
+    <div className="video-list-entry">
+      <div className="media-left media-middle">
+        <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title">Video Title</div>
+        <div className="video-list-entry-detail">Video Description</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  );
+
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
